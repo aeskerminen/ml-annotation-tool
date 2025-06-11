2c93840c03bc255065f00cf910a1547486636162
Artturi Kerminen <kerminenartturi@gmail.com>
2025-06-11 22:56:43 +0300

    prototyping...

--- ml-annotation-tool-frontend/src/Attributes.tsx
+++ ml-annotation-tool-frontend/src/Attributes.tsx
@@ -1,32 +1,54 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { type RootState } from "./store";
-import {add, remove} from "./attributeSlice"
+import { add, remove } from "./attributeSlice"
 
 const Attributes = () => {
+    const attributes = useSelector((state: RootState) => state.attributes.value);
+    const dispatch = useDispatch();
+    const [attributeInput, setAttributeInput] = useState<string>("");
 
-    const attributes = useSelector((state: RootState) => state.attributes.value)
-    const dispatch = useDispatch()
+    const handleAddAttribute = () => {
+        const trimmed = attributeInput.trim();
+        if (trimmed) {
+            dispatch(add(trimmed));
+            setAttributeInput("");
+        }
+    };
 
-    const [attributeInput, setAttributeInput] = useState<string>();
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAttributeInput(e.target.value);
+    };
 
     return (
-        <div className='flex-1 text-center bg-gray-600 flex flex-col p-4'>
+        <div className="flex-1 text-center bg-gray-600 flex flex-col p-4">
             <p className="flex-0 text-center p-1 bg-black">Attributes</p>
             <div className="flex-0 p-2 bg-gray-400 flex flex-row justify-between gap-2">
-                <input className="p-2 flex-1" type="text" id="attribute-input" placeholder="Atrribute..." onInput={(e) => setAttributeInput(e.target.value)} value={attributeInput}></input>
-                <button onClick={() => {
-                    dispatch(add(attributeInput))
-                    setAttributeInput("");
-                }}>Add attribute</button>
+                <input
+                    className="p-2 flex-1"
+                    type="text"
+                    id="attribute-input"
+                    placeholder="Attribute..."
+                    onChange={handleInputChange}
+                    value={attributeInput}
+                    onKeyDown={e => { if (e.key === 'Enter') handleAddAttribute(); }}
+                    autoComplete="off"
+                />
+                <button onClick={handleAddAttribute} disabled={!attributeInput.trim()}>
+                    Add attribute
+                </button>
             </div>
             <div className="bg-white p-2 flex-1 flex flex-col gap-2">
-                {attributes.map((e, i) => {
-                    return <p key={i} className="bg-black p-2 text-medium">{e}</p>
-                })}
+                {attributes.length === 0 ? (
+                    <p className="text-gray-500">No attributes yet.</p>
+                ) : (
+                    attributes.map((attr: string, i: number) => (
+                        <p key={attr + i} className="bg-black p-2 text-medium text-white">{attr}</p>
+                    ))
+                )}
             </div>
         </div>
-    )
+    );
 }
 
 export default Attributes
